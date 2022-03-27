50a49b5cc5ef4123c8b24c17989a61c9ef8effaf
Chris Bain <baincd@users.noreply.github.com>
2022-03-27 11:15:19 -0400

    Add folding regions for downloading/downloaded lines

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,38 @@
 import * as vscode from 'vscode';
 
+const downloadingLinesRegEx = /^(\[INFO\] )?Download(ing|ed) from \w*:/
+const downloadingProgressLineRegEx = /^Progress \(\d+\): /
+const whitespaceLineRegEx = /^\s*$/
 
 class MavenLogFoldingRangeProvider implements vscode.FoldingRangeProvider {
     onDidChangeFoldingRanges?: vscode.Event<void> | undefined;
-
-
+    
     provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
+        let foldingRanges: vscode.ProviderResult<vscode.FoldingRange[]> = [];
 
+        let downloadingLinesStartIdx: (number | undefined) = undefined
+        let downloadingProgressLinesStartIdx: (number | undefined) = undefined
+
+        for (let lineIdx = 0; lineIdx < document.lineCount; lineIdx++) {
+            const lineText = document.lineAt(lineIdx).text;
+
+            if (downloadingLinesStartIdx === undefined && downloadingLinesRegEx.test(lineText)) {
+                downloadingLinesStartIdx = lineIdx
+            } else if (downloadingLinesStartIdx !== undefined && !downloadingLinesRegEx.test(lineText) && !downloadingProgressLineRegEx.test(lineText) && !whitespaceLineRegEx.test(lineText)) {
+                foldingRanges.push(new vscode.FoldingRange(downloadingLinesStartIdx,lineIdx));
+                downloadingLinesStartIdx = undefined;
+            }
+
+            if (downloadingProgressLinesStartIdx === undefined && downloadingProgressLineRegEx.test(lineText)) {
+                downloadingProgressLinesStartIdx = lineIdx
+            } else if (downloadingProgressLinesStartIdx !== undefined && !downloadingProgressLineRegEx.test(lineText) && !whitespaceLineRegEx.test(lineText)) {
+                foldingRanges.push(new vscode.FoldingRange(downloadingProgressLinesStartIdx,lineIdx-1));
+                downloadingProgressLinesStartIdx = undefined;
+            }
+
+        }
+
+        return foldingRanges;
     }
 
 }
